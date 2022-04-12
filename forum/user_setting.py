31dd7aec78e640df19c8f3a8fdba6aba1ed8b61e
Joseph Kowalski <jkjr25@gmail.com>
2022-04-12 19:05:12 -0400

    Fixed merge conflicts

--- forum/user_setting.py
+++ forum/user_setting.py
@@ -5,8 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 from forum.model import *
 from forum.forum import *
-from forum.log_in_out import *
-from forum.create_account import *
 
 
 @login_required
