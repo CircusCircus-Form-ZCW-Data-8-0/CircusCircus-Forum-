f91a81b27dbf3781a69e274f3c285f18521c18e2
Jason <leejason1990@gmail.com>
2022-04-12 22:39:13 -0400

    minor updates

--- forum/app.py
+++ forum/app.py
@@ -8,6 +8,6 @@ app.config.update(
     SITE_NAME="Data Cohort",
     SITE_DESCRIPTION="A Data Engineer Forum",
     SQLALCHEMY_DATABASE_URI='sqlite:////tmp/database.db',
-    COPYRIGHT='DATA 3.0-8.0? © 2022'
+    COPYRIGHT='ZCW DATA 3.0-8.0? © 2022'
 
 )
