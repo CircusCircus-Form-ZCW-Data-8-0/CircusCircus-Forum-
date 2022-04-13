d5cf1bcc4b3a4fb40e2ba0087c0e884f293d2189
Jason <leejason1990@gmail.com>
2022-04-12 22:34:39 -0400

    header added

--- forum/app.py
+++ forum/app.py
@@ -5,7 +5,7 @@ app.config.update(
     TESTING=True,
     SECRET_KEY=b'kristofer',
 
-    SITE_NAME="Data Cohort1",
+    SITE_NAME="Data Cohort",
     SITE_DESCRIPTION="A Data Engineer Forum",
     SQLALCHEMY_DATABASE_URI='sqlite:////tmp/database.db',
     COPYRIGHT='DATA 3.0-8.0? © 2022'
