9a4f047c13cd8a90fad846820cf09cced03c8ed9
Vandana <vandana@Zipcoders-MBP.fios-router.home>
2022-04-14 16:24:37 -0400

    added db column name image_file

--- forum/user_setting.py
+++ forum/user_setting.py
@@ -6,10 +6,12 @@ from flask_sqlalchemy import SQLAlchemy
 from forum.model import *
 from forum.forum import *
 
-@login_required
-@app.route('/action_account')
-def action_account():
 
-    # image_file=url_for('static',filename='profile/' + current_user.image_file)  # this is current user image from location
-   # image_file = url_for('static', + current_user.image_file )
-    return render_template('account.html' )#,image_file=image_file) #assigning image_file to db
+@login_required
+@app.route('/action_profile')
+def action_profile():
+
+    image_file=url_for('static',filename='/' + str(current_user.image_file))  # this is current user image from location
+    return render_template('account.html' ,image_file=image_file,form=form) #assigning image_file to account.html
+
+
