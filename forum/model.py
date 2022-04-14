46b60c6f31e6c7f2fa3ff794caeec646bcddd092
Jason <leejason1990@gmail.com>
2022-04-14 17:45:33 -0400

    bootstrap changes

--- forum/model.py
+++ forum/model.py
@@ -26,7 +26,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.Text, unique=True)
     password_hash = db.Column(db.Text)
     email = db.Column(db.Text, unique=True)
-    image_file=db.Column(db.Text,default='profile.jpeg')
+    #image_file=db.Column(db.Text,default='profile.jpeg')
     #admin = db.Column(db.Boolean, default=False, unique=True)
     posts = db.relationship("Post", backref="user")
     comments = db.relationship("Comment", backref="user")
