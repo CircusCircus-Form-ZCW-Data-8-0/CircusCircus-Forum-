1a5761b1f33dcad6721cfc85d17996eac8bf6051
Joseph Kowalski <jkjr25@gmail.com>
2022-04-15 15:13:53 -0400

    Working on getting links to work in string

--- forum/links.py
+++ forum/links.py
@@ -1,15 +1,24 @@
-
 from urllib.parse import urlparse, urlsplit
+import re
 
 def links(content):
-    url = urlparse(content)
-    if (url.scheme and url.netloc and url.path is not None) and len(url.netloc.split('.')) > 1:
-        url1 = ''
-        link_string = '<a href = "'
-        link_string = link_string + content
-        link_string = link_string + '">'
-        link_string = link_string + content + '</a><br>\n'
-        url1 += link_string
-        return url1
-    else:
-        return content
+    split_content = re.split(r'(\s+)', content)
+    new_string = ''
+    for word in split_content:
+        url = urlparse(word)
+        if (url.scheme and url.netloc and url.path is not None) and len(url.netloc.split('.')) > 1 \
+                and url.scheme in ['http', 'https']:
+            url1 = ''
+            link_string = '<a href = "'
+            link_string = link_string + word
+            link_string = link_string + '">'
+            link_string = link_string + word + '</a><br>\n'
+            url1 += link_string
+            new_string += url1
+        else:
+            new_string += word
+    return new_string
+#
+# content = 'this link      is https://yoda.zipcode.rocks/apache-spark-for-zcw-data/'
+#
+# print(links(content))
