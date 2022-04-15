970034acf4c22e0ce42d54888ce684cc6683e5a0
Joseph Kowalski <jkjr25@gmail.com>
2022-04-15 08:39:38 -0400

    Changed from regex to urlparse to catch additional edge cases

--- forum/links.py
+++ forum/links.py
@@ -1,16 +1,14 @@
-import re
-
+from urllib.parse import urlparse, urlsplit
 
 def links(content):
-    new_content = ''
-    pattern = re.compile(r'(https?://[^\s]+)')
-    if re.search(pattern, content) is not None:
-        word2 = re.search(pattern, content).group(1)
+    url = urlparse(content)
+    if (url.scheme and url.netloc and url.path is not None) and len(url.netloc.split('.')) > 1:
+        url1 = ''
         link_string = '<a href = "'
-        link_string = link_string + word2
+        link_string = link_string + content
         link_string = link_string + '">'
-        link_string = link_string + word2 + '</a><br>\n'
-        new_content += link_string
-        return new_content
+        link_string = link_string + content + '</a><br>\n'
+        url1 += link_string
+        return url1
     else:
         return content
